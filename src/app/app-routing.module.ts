170f0f8b3e6fb928d0f91d41552e182e1fe6b481
riizkyfauzan <iam.rizkyfauzan@gmail.com>
2020-11-28 21:03:59 +0700

    confirm-password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   Shell.childRoutes([{ path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) }]),
   Shell.childRoutes([{ path: 'register', loadChildren: () => import('./register/register.module').then((m) => m.RegisterModule) }]),
   Shell.childRoutes([{ path: 'password', loadChildren: () => import('./password/password.module').then((m) => m.PasswordModule) }]),
+  Shell.childRoutes([{ path: 'new', loadChildren: () => import('./new/new.module').then((m) => m.NewModule) }]),
   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
