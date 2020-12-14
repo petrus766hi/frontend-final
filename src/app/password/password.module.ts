61ac94116a35e9a3100a92ebe3b899a234d98a3f
petrus766hi <petrus.sigiro@gmail.com>
2020-12-14 16:17:00 +0700

    update_again

--- src/app/password/password.module.ts
+++ src/app/password/password.module.ts
@@ -7,9 +7,17 @@ import { PasswordRoutingModule } from './password-routing.module';
 import { PasswordComponent } from './password.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-
+import { NgxLoadingModule } from 'ngx-loading';
 @NgModule({
-  imports: [CommonModule, TranslateModule, PasswordRoutingModule, NgbModule, SharedModule, FormsModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    PasswordRoutingModule,
+    NgbModule,
+    SharedModule,
+    FormsModule,
+    NgxLoadingModule,
+  ],
   declarations: [PasswordComponent],
 })
 export class PasswordModule {}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { ProfileService } from '../services/profile.service';
 import { environment } from '@env/environment';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Profile } from '../models/profile';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
@@ -10,19 +11,27 @@ import { Profile } from '../models/profile';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  version: string | null = environment.version;
-
   profiles: any = Profile;
   id: any = '';
   constructor(public profileservice: ProfileService, private router: ActivatedRoute, public route: Router) {
     this.id = localStorage.getItem('id');
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userProfile();
+    console.log('xxx', this.id);
+  }
 
   userProfile() {
-    this.profileservice.getProfile(this.id).subscribe((response: any) => {
-      this.profiles = response.data;
-    });
+    this.profileservice
+      .getProfile(this.id)
+      .pipe(
+        finalize(() => {
+          console.log('done');
+        })
+      )
+      .subscribe((response: any) => {
+        this.profiles = response.data;
+      });
   }
 }
