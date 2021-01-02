42ca198dbcc24fc3bfeefa0aaa975c3c9ac00312
riizkyfauzan <iam.rizkyfauzan@gmail.com>
2021-01-02 23:43:11 +0700

    update

--- src/app/lomba/winner/winner.component.ts
+++ src/app/lomba/winner/winner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { environment } from '@env/environment';
+import { WinnerService } from '../../services/winner.service';
 
 @Component({
   selector: 'app-winner',
@@ -6,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./winner.component.scss'],
 })
 export class WinnerComponent implements OnInit {
-  constructor() {}
+  public winnerLomba: any = {};
+  id: any = '';
+  constructor(public route: Router, public winnerservice: WinnerService) {}
 
   ngOnInit(): void {}
+
+  updateWinner() {
+    this.winnerservice.inputWinner(this.winnerLomba, this.id).subscribe((response: any) => {
+      if (response.success) console.log('>>>>>>>>>', response);
+    });
+  }
 }
